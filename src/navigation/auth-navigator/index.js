29f149ec01badeb5327258105c4fe58935f0e810
UmairSprintX <umairamin@macs-MacBook-Pro.local>
2022-08-04 20:00:21 +0500

    AuthNavigator

--- src/navigation/auth-navigator/index.js
+++ src/navigation/auth-navigator/index.js
@@ -1,10 +1,22 @@
-import {Text, View} from 'react-native';
 import React from 'react';
+
+import {createStackNavigator} from '@react-navigation/stack';
+
+import SCREEN from 'utils/constants';
+import Login from 'screens/auth/login';
+import Signup from 'screens/auth/signup';
+
+const Stack = createStackNavigator();
+const {LOGIN, SIGNUP} = SCREEN;
 
 export default function AuthNavigator() {
   return (
-    <View>
-      <Text>AuthNavigator</Text>
-    </View>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Stack.Screen name={LOGIN} component={Login} />
+      <Stack.Screen name={SIGNUP} component={Signup} />
+    </Stack.Navigator>
   );
 }
