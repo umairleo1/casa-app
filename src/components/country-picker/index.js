bc4caf819b9a46c88714b693ccbb3c3f3a20e53c
Suleman7041 <suleman.amjad@sprintx.net>
2022-09-01 19:33:16 +0500

    push new add in profile

--- src/components/country-picker/index.js
+++ src/components/country-picker/index.js
@@ -10,25 +10,33 @@ import CountryPicker,{DEFAULT_THEME} from 'react-native-country-picker-modal'
 
 export default function CountryPickerModal() {
     const [value,setValue]=React.useState('');
+    const [countryCode, setCountryCode] = React.useState('US')
+
+    const onSelect = (Country) => {
+      setCountryCode(Country.cca2);
+      setValue(Country)
+      // props.setSelectCountryCode(Country.callingCode[0]);
+    };
 
   return (
     <>
-    {/* {alert(JSON.stringify(value.name))} */}
+    {/* {alert(JSON.stringify(value))} */}
       <View style={styles.mainView}>
       <CountryPicker
         withFlag={true}
-        withFilter
-        withCountryNameButton
         withCallingCode={true}
+        // withCallingCode={true}
+        withFlagButton
+        countryCode={countryCode}
         containerButtonStyle={styles.pickerButtonStyle}
-        onSelect={(Country) => setValue(Country)}
+        onSelect={(Country) => onSelect(Country)}
         theme={{
           ...DEFAULT_THEME,
           backgroundColor: colors.whiteColor,
           onBackgroundTextColor: colors.black,
           fontSize: 15,
         }}
-        // placeholder={'Sélectionnez un pays'}
+        placeholder={'Select Country'}
         // onValueChange={(value)=>setValue(value)}
       />
      {value &&
@@ -45,13 +53,14 @@ const styles = StyleSheet.create({
   mainView: {
     borderWidth:1,
     borderColor: colors.innerBorder,
-    paddingHorizontal: wp(3),
+    paddingHorizontal: wp(1),
     flexDirection:'row',
     borderRadius:2
   },
   pickerButtonStyle:{
-    backgroundColor:'red',
+    // backgroundColor:'red',
     height:40,
+    padding:5
   },
   text:{
    alignSelf:'center'    
